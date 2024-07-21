3c03566a9ce2f312f648fc0d09f183ec8cd0bcd6
Pedro A. Pajarillo Jr <pedropa140@gmail.com>
2024-07-21 03:16:14 -0400

    20240721T03:16:05 - Maybe final product for /bereal

--- bot.py
+++ bot.py
@@ -82,8 +82,8 @@ def run_discord_bot():
                 print(f'NEW PING TIME: \t\t{random_datetime}')
             
             print('--------------------------------------------------')
-            # if current_datetime == random_datetime:
-            if current_datetime == current_datetime:
+            if current_datetime == random_datetime:
+            # if current_datetime == current_datetime:
                 for guild in bot.guilds:
                     print(f'Guild Name: {guild.name}')
                     for channel in guild.channels:
